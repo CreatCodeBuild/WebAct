ae1d9121b0ecb051013084547248b06c823fe03f
CreatCodeBuild <wangxuanzhealbert@gmail.com>
2016-07-20 16:00:01 +0800

    + class Inputs

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,21 +1,34 @@
-function init() {
-  var img = new Image();
+function process_image() {
+  let img = new Image();
   img.src = document.getElementById('ocr').src;
   console.log(img);
-
   img.onload = function() {
-      Tesseract.recognize(img,
-                          {
-                            progress: function(e){
-                              console.log(e)
-                            },
-                            lang: 'chi_sim'
-                          }
-      ).then( function(d){ document.getElementById('display').innerHTML+=d.text } )
+    Tesseract.recognize(img,
+                        {
+                          progress: function(e){
+                            console.log(e)
+                          },
+                          lang: 'chi_sim'
+                        }
+    ).then(function(d) {
+      document.getElementById('display').innerHTML += d.text;
+    });
   }
 }
 
+function set_image_view(file) {
+  let imageElement = document.getElementById('ocr');
+  imageElement.src = file;
+}
+
 window.onload = function() {
-  console.log('window.onload')
-  init();
+  console.log('window.onload');
+  let element = document.getElementById('select_file');
+  Inputs.select_file(element, function(err, file, url) {
+    if(err === undefined) {
+      console.log(file, url);
+    } else {
+      console.log(err);
+    }
+  });
 }
---
+++ public/javascripts/inputs.js
@@ -0,0 +1,21 @@
+let InputsTag = 'Inputs::';
+class Inputs {
+  static select_file(element, on_file_selected) {
+    element.addEventListener('change', function(event) {
+      let file = event.target.files[0];
+      let error = undefined;
+      let url;
+      if(file) {
+        console.log(InputsTag+file.name);
+        url = URL.createObjectURL(file);
+      } else {
+        error = 'no file is selected';
+      }
+      on_file_selected(error, file, url);
+    });
+  }
+
+  static use_camera(callback) {
+
+  }
+}
