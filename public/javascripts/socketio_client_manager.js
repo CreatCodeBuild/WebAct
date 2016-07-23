291ccd81a46946cf9ec0add7349bfabf51242ecb
CreatCodeBuild <wangxuanzhealbert@gmail.com>
2016-07-23 19:44:21 +0800

    the more js I write, the worse I feel. Thank God that I get Python in this project

--- public/javascripts/socketio_client_manager.js
+++ public/javascripts/socketio_client_manager.js
@@ -1,6 +1,7 @@
 const EVENT = {
 	FILE_UPLOAD: 'file upload',
-	DONE_UPLOAD: 'done upload'
+	DONE_UPLOAD: 'done upload',
+	PROCESSED_IMAGE: 'processed image'
 }
 
 var socket = io.connect('http://localhost:8080');
@@ -16,41 +17,17 @@ class SocketioClientManager {
     ss.createBlobReadStream(file).pipe(stream);
 	}
 
-	// emit_video_stream(file) {
-	// 	console.log('begin emit_video_stream');
-	// 	var stream = ss.createStream();
-	// 	console.log('var stream = ss.createStream();');
-  //   // upload a file to the server.
-  //   ss(this.socket).emit(
-  //     EVENT.FILE_UPLOAD,
-  //     stream,
-  //     {
-  //       size: file.size,
-  //       name: file.name
-  //     }
-  //   );
-  //   var blobStream = ss.createBlobReadStream(file);
-	// 	blobStream.pipe(stream);
-	//
-  //   //track progress
-  //   // var size = 0;
-  //   // blobStream.on('data', function(chunk) {
-  //   //     size += chunk.length;
-  //   //     console.log(Math.floor(size / file.size * 100) + '%');
-  //   // });
-	//
-	//
-	//
-	// 	var stream = ss.createStream();
-	// 	// upload a file to the server.
-	// 	ss(socket).emit('file', stream, {size: file.size});
-	// 	ss.createBlobReadStream(file).pipe(stream);
-	//
-	// 	console.log('end emit_video_stream');
-	// }
+	/*
+		register all on-event callbacks
+	*/
+	static init() {
+		socket.on(EVENT.DONE_UPLOADING, function() {
+			// todo: implement it
+		});
 
-	on_done_upload(callback) {
-		this.socket.on(EVENT.DONE_UPLOADING, callback);
+		socket.on(EVENT.PROCESSED_IMAGE, function() {
+			// todo: implement it
+		})
 	}
 
 }
