8c6c1460417ff75cafe337b520d461f45ebbf7e9
CreatCodeBuild <wangxuanzhealbert@gmail.com>
2016-07-23 16:58:16 +0800

    ~

--- socketio_server_manager.js
+++ socketio_server_manager.js
@@ -1,11 +1,13 @@
 var socketio = require('socket.io');
 var ss = require('socket.io-stream');
-var fs = require('fs');
+// var fs = require('fs');
 
 const EVENT = {
 	FILE_UPLOAD: 'file upload',
-	DONE_UPLOAD: 'done upload',
+	DONE_UPLOAD: 'done upload'
 }
+
+const TAG = 'ServerSocketManager';
 
 class ServerSocketManager {
 
@@ -17,16 +19,14 @@ class ServerSocketManager {
 		var io = this.io;
 		io.on('connection', function(socket){
 		  console.log('a user connected');
-		  socket.broadcast.emit('Hi everybody!');
-		  socket.join();
+		  // socket.join();
 
 		  socket.on('disconnect', function() {
 		  	console.log('user disconnected');
 		  });
 
 		  ss(socket).on(EVENT.VIDEO_UPLOAD, function(stream, data) {
-		    console.log('video: ' + data.name);
-		    var fileName = "video"+Date.now().toString();
+		    console.log('TAG: ' + data.name);
   			var size = 0;
   			stream.on('data', function(chunk) {
   				size += chunk.length;
