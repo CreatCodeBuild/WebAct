1887c85895661c764ff4e58b3ee696593044160b
CreatCodeBuild <wangxuanzhealbert@gmail.com>
2016-07-23 17:04:57 +0800

    + terrible shit code

--- public/javascripts/socketio_client_manager.js
+++ public/javascripts/socketio_client_manager.js
@@ -33,14 +33,6 @@ class SocketManager {
     blobStream.on('data', function(chunk) {
         size += chunk.length;
         console.log(Math.floor(size / file.size * 100) + '%');
-        if(size === file.size) {
-        	$('#upload_video').html(
-        	"<a>" +
-          		"<span class='mif-cloud-upload icon'></span>" +
-            	" Upload Video" +
-        	"</a>"
-      	);
-        }
     });
 
     blobStream.pipe(stream);
