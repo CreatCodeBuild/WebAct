f802caae7fa75189a4e968f43e40a4dd6ecc144d
CreatCodeBuild <wangxuanzhealbert@gmail.com>
2016-07-31 18:36:24 +0800

    now I see

--- image_processing_module/sd.py
+++ image_processing_module/sd.py
@@ -4,6 +4,7 @@ import cv2
 import numpy as np
 import sys
 import zerorpc
+import StringIO
 
 
 def gray(im):
@@ -60,6 +61,32 @@ def run():
     cv2.imwrite('temp.jpg', threshed)
 
 
+def process_an_image(image):
+    # cv2.imshow('', image)
+    # cv2.waitKey()
+    grayed = gray(image)
+    print(1)
+    threshed = threshold(~grayed, 'mean')
+    print(2)
+    return threshed
+
+
+def remove_lines(threshed_image):
+    """
+    :param threshed_image: OpenCV image
+    :return: void, this function writes on threshed_image
+    """
+    v_lines, h_lines = line_detect(threshed_image)
+    color = (0, 0, 0)  # color to override on threshed image, in order to cover lines
+    for line in v_lines:
+        for x1, y1, x2, y2 in line:
+            cv2.line(threshed_image, (x1, y1), (x2, y2), color, 3)
+    for line in h_lines:
+        for x1, y1, x2, y2 in line:
+            cv2.line(threshed_image, (x1, y1), (x2, y2), color, 3)
+
+
+
 class StreamingRPC():
     @zerorpc.stream
     def process_image(self, image_buffer):
@@ -71,33 +98,29 @@ class StreamingRPC():
         # it provides useful info
         # convert raw binary data to OpenCV acceptable image object
 
-
+        # print to debug
         print('type:', type(image_buffer), 'len:', len(image_buffer))
         for c in image_buffer:
             print('type of element:', type(c))
+        # debug end
+
+        # construct image from binary buffer/file stream or whatever abstraction you see
         image_buffer = ''.join(image_buffer)
         np_array = np.frombuffer(image_buffer, dtype='uint8')
         image = cv2.imdecode(np_array, cv2.IMREAD_COLOR)
-        cv2.imshow('', image)
-        cv2.waitKey()
-        grayed = gray(image)
-        print(1)
-        threshed = threshold(~grayed, 'mean')
-        print(2)
-        v_lines, h_lines = line_detect(threshed)
-        color = (0, 0, 0)
-        for line in v_lines:
-            for x1, y1, x2, y2 in line:
-                cv2.line(threshed, (x1, y1), (x2, y2), color, 3)
-        for line in h_lines:
-            for x1, y1, x2, y2 in line:
-                cv2.line(threshed, (x1, y1), (x2, y2), color, 3)
-        retval, buffer = cv2.imencode('.jpg', threshed)
-        print('type:', type(buffer), 'len:', len(buffer))
+
+        # process the received image
+        threshed = process_an_image(image)
+        remove_lines(threshed)
+
+        retval, buffer_of_image = cv2.imencode('.jpg', threshed)
+
+        print('type:', type(buffer), 'len:', len(buffer))  # debug
+
         # need to convert numpy ndarray to python list
         # or any zerorpc seriallizable object
         # int8 list?
-        tostring = buffer.tostring()
+        tostring = buffer_of_image.tostring()
         print('len:', len(tostring))
         return tostring
 
@@ -109,28 +132,38 @@ def server_up():
 
 
 if __name__ == '__main__':
-    server_up()
+    # server_up()
 
     # debug purpose
-    # image = cv2.imread(str(sys.argv[1]))
-    # grayed = gray(image)
-    # threshed = threshold(~grayed, 'mean')
-    #
-    # v_lines, h_lines = line_detect(threshed)
-    #
-    # color = (0, 0, 0)
-    # for line in v_lines:
-    #     for x1, y1, x2, y2 in line:
-    #         cv2.line(threshed, (x1, y1), (x2, y2), color, 3)
-    # for line in h_lines:
-    #     for x1, y1, x2, y2 in line:
-    #         cv2.line(threshed, (x1, y1), (x2, y2), color, 3)
-    #
-    # cv2.imshow('lines', threshed)
+    # process the received image
+    image = cv2.imread('demo4.jpg')
+    threshed = process_an_image(image)
+    remove_lines(threshed)
+    # cv2.imshow('', threshed)
     # cv2.waitKey()
-    # cv2.imwrite('lineremoved.jpg', threshed)
 
-    # didn't find out why this didn't work
-    # retval, buffer = cv2.imencode('.png', threshed)
-    # for b in buffer:
-    #     sys.stdout.write(b)
+    retval, buffer_of_image = cv2.imencode('.jpg', threshed, [cv2.IMWRITE_JPEG_QUALITY, 100])
+    print(retval)
+
+    print('type:', type(buffer_of_image), 'len:', len(buffer_of_image))  # debug
+
+    # need to convert numpy ndarray to python list
+    # or any zerorpc seriallizable object
+    # int8 list?
+    tostring = buffer_of_image.tostring()
+
+    with open('temp.jpg', 'w') as f:
+        f.write(buffer_of_image)
+
+    # Close object and discard memory buffer --
+    # .getvalue() will now raise an exception.
+    # output.close()
+
+    #decode from jpeg format
+    decimg = cv2.imdecode(buffer_of_image,1)
+
+    cv2.imshow('Source Image', threshed)
+    cv2.imshow('Decoded image', decimg)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
