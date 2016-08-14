93d6ab5bb2b45b8285be609f2b3e6e30f14b140d
CreatCodeBuild <wangxuanzhealbert@gmail.com>
2016-08-14 20:58:54 +0800

    compressed file

--- FlaskVersion/public/javascripts/index.js
+++ FlaskVersion/public/javascripts/index.js
@@ -88,16 +88,36 @@ window.onload = function Main() {
     }
   }
 
+  function dataURItoBlob(dataURI) {
+    // convert base64/URLEncoded data component to raw binary data held in a string
+    var byteString;
+    if (dataURI.split(',')[0].indexOf('base64') >= 0)
+      byteString = atob(dataURI.split(',')[1]);
+    else
+      byteString = unescape(dataURI.split(',')[1]);
+
+    // separate out the mime component
+    var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
+
+    // write the bytes of the string to a typed array
+    var ia = new Uint8Array(byteString.length);
+    for (var i = 0; i < byteString.length; i++) {
+      ia[i] = byteString.charCodeAt(i);
+    }
+
+    return new Blob([ia], {type:mimeString});
+  }
+
   function compressed_image_onload() {
     //this should be the compressed result image
-    console.log(this);
+    var compressedFile = dataURItoBlob(this.src);
+    console.log(compressedFile);
   }
 
   function image_element_onload() {
     console.log('image_element_onload');
 
     var resultImage = webAct.compress_image(document.getElementById('ocr'), 50);
-    console.log(resultImage);
     resultImage.onload = compressed_image_onload;
 
   }
